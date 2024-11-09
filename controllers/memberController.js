ca5edd8bec773125ed78945b8044373abfec0f23
Gasamagera <iradukundaprince561@gmail.com>
2024-11-09 13:33:52 +0000

    Filtiring On Api Based On Some specificcation

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -6,3 +6,22 @@ exports.createMember = factory.createOne(Member);
 exports.updateMember = factory.updateOne(Member);
 exports.getAllMembers = factory.getAll(Member);
 exports.deleteMember = factory.deleteOne(Member);
+
+exports.getMembersByFilters = catchAsync(async (req, res, next) => {
+  const member = await Member.aggregate([
+    {
+      $match: {
+        age: { $gte: 30 },
+        maritalStatus: { $regex: /^widowed$/, $options: "i" },
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: "Success",
+    results: member.length,
+    data: {
+      member,
+    },
+  });
+});
--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -4,12 +4,16 @@ const authController = require("./../controllers/authController");
 const { route } = require("./adminRoutes");
 const router = express.Router();
 
+router.use(authController.protect);
+
+router.route("/urgent-member").get(memberController.getMembersByFilters);
+
 router
   .route("/")
-  .get(authController.protect, memberController.getAllMembers)
+  .get(memberController.getAllMembers)
   .post(memberController.createMember);
 router
   .route("/:id")
   .patch(memberController.updateMember)
-  .delete(authController.protect, memberController.deleteMember);
+  .delete(memberController.deleteMember);
 module.exports = router;
